16da963d24edd2a9671c2d128317bd5bd5123c60
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-06 15:52:12 +0200

    get PostList ok

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const posts = [
-  { id: 1, title: 'Premier article', content: 'Contenu du premier article' },
-  { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article' },
-  // Ajoutez d'autres articles ici
-];
-
 function PostList() {
+const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/post');
+      if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des la liste des articles');
+
+      }
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  fetchPosts();
+},[])
+
+
   return (
-    <div>
-      {posts.map(post => (
-        <div key={post.id}>
-          <h3><Link to={`/post/${post.id}`}>{post.title}</Link></h3>
-          <p>{post.content}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
+
+    <div className="post-list">
+      <h2>Articles</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={nanoid()}>
+            <Link to={`/post/${post.id}`}>{post.message}{post.author}{post.likers}</Link>
+          </li>
+        ))}
+        </ul>
+</div>
+);
+  }
 
 export default PostList;

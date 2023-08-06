3ea343baf6df4255396373a2a77e0e4ca13dd30c
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-06 11:53:20 +0200

    init react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import Navbar from './components/NavBar';
+import { Home } from './components/pages/Home';
+import { NewPost } from './components/pages/NewPost';
+import { SinglePost} from './components/pages/SinglePost';
 
-function App() {
-
+export const App = () => {
   return (
     <>
-    <h1>Test</h1>
+    <BrowserRouter>
+    <div className="wrapper">
+
+    <Navbar />
+    <Header />
+    <main>
+      <Routes> 
+        <Route path="/" element={<Home/>} />
+        <Route path="/newPost" element={<NewPost />} />
+        <Route path="/singlePost" element={<SinglePost />} />
+      </Routes>
+    </main>
+    
+    </div>
+    </BrowserRouter>
+    
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import logo from '../path/to/your/logo.png';
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          {/* <img src={logo} alt="Logo" width="50" height="50" /> */}
+          My Blog
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/new">
+                Nouvel article
+              </Link>
+            </li>
+            {/* Ajoutez d'autres liens de navigation ici */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

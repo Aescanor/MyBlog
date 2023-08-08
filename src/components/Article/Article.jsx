dbe47cb28ab65b998cbf1d9adb53ddb12d59c37f
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-08 18:31:11 +0200

    ended update post logic and sass

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,32 +2,37 @@ import PropTypes from 'prop-types';
 import './Article.scss';
 import FormattedDate from '../FormattedDate';
 import SVGs from '../Svgs';
+import { Link } from 'react-router-dom';
 
 export function Article({post}){
       
   return (
     <div className="post-detail">
         <h2>{post.title}</h2>
+
         <figure>
         <img src={post.picture} alt={post.title} />
         <figcaption>{post.title}</figcaption>
         </figure>
+
         <div className="content">
         <p>{post.message}</p>
         </div>
+
         <div className="infos">
         <p> <strong>Date de publication:</strong> <FormattedDate date={post.createdAt}/></p>
         <p><strong>Auteur:</strong> {post.author}</p>
         </div>
+
         <div className="likes">
         <img src={SVGs.LikeIcon} alt="like icon"/>
         <p>{post.likers.length}</p>
 
         {/* futures fonctionnalités : */}
-        {/* use UseParams to by pas by url the id of the post to delete or edit */}
-        <button className='delete-button'>Supprimer</button>
-        <button className='edit-button'>Modifier</button>
-
+        <div className="extra-btns"> 
+        <Link to={`/deletePost/${post._id}`}><button className='delete-btn'>Supprimer</button></Link>
+        <Link to={`/editPost/${post._id}`}><button className='edit-btn'>Modifier</button></Link>
+      </div>
         </div>
 
     </div>
@@ -37,6 +42,7 @@ export function Article({post}){
 // ProptTypes  : pour ne plus avoir d'erreur dans la console
 Article.propTypes = {
     post: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       picture: PropTypes.string.isRequired,
       message: PropTypes.string.isRequired,
